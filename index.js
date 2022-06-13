fbfe8619d32c6d07d8a35616dec44bd3a8f2a58e
Kipruto Duncan <kiprutoduncan36@gmail.com>
2022-06-13 17:26:13 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(ride=100){
+  
+  
+  if (ride<=400){
+    return 'This one is on me!'
+  }else if (ride>2000&& ride<2500){
+    return 'I will gladly take your thirty bucks.'
+  }else if(ride>2500){
+    return 'No can do.'
+
+  }
+    
+
+  }
+
+
+
+
+function ternaryCheckCity(city){
+  return city==='NYC'?'Ok, sounds good.':'No go.'
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip( tip='generous'){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+      case 'not as generous':
+        return 'Thank you.';
+        break;
 
-function switchOnCharmFromTip(){
+      default:
+        return 'Bye.';
+        break;
+  }
   // Write your code here!
 }
